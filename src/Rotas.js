afdf91c8fcdac0922646143cb840cfc3cab496f1
Jefferson <jeffersonbds_barros@hotmail.com>
2019-06-20 16:08:10 -0300

    Jogo jokenpo implementado

--- src/Rotas.js
+++ src/Rotas.js
@@ -3,13 +3,15 @@ import {Router, Scene, Stack, StyleSheet} from 'react-native-router-flux';
 
 import TelaInicial from './TelaInicial';
 import Http from './components/Http/Http';
+import Jogo from './components/Jokenpo/Jogo';
 
 const Rotas = () => (
 
   <Router>
     <Stack key="root">
       <Scene key='telainicial' component={TelaInicial} title="Tela Inicial" initil />
-      <Scene key='http' component={Http} title="HTTP" />
+      <Scene key='http' component={Http} title="Requisições HTTP" />
+      <Scene key='jokenpo' component={Jogo} title="Jogo Jokenpo" />
     </Stack>
   </Router>
 
--- src/TelaInicial.js
+++ src/TelaInicial.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Button} from 'react-native';
+import {View, Button, Text, StyleSheet} from 'react-native';
 import {Actions} from 'react-native-router-flux';
 
 export default class Principal extends Component {
@@ -7,13 +7,38 @@ export default class Principal extends Component {
     return (
       <View>
 
-        <Button
-          onPress={() => { Actions.http(); }}
-          title="HTTP"
-          color="#FF8C00"
-        />
+        <Text style={styles.textoTitulo}> Escolha uma opção </Text>
+
+        <View style={styles.distanciaBotoes}>
+          <Button
+            onPress={() => { Actions.http(); }}
+            title="Requisições HTTP com Axios"
+            color="#FF8C00"
+          />
+        </View>
+
+        <View style={styles.distanciaBotoes}>
+          <Button
+            onPress={() => { Actions.jokenpo(); }}
+            title="Jogo Jokenpo"
+            color="#FFB90F"
+          />
+        </View>
 
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  textoTitulo: {
+    fontSize: 20,
+    color: 'black',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    padding: 30
+  },
+  distanciaBotoes: {
+    padding: 25
+  }
+});
